0f7bc9e5112accd35de76d78c3e3b5b822e2a636
Case Larsen <clarsen@gmail.com>
2018-08-24 13:33:24 -0700

    move to remote influxDB, move to use PID control

--- monitor_temp.py
+++ monitor_temp.py
@@ -1,27 +1,35 @@
-import os
 import glob
 import time
 import RPi.GPIO as GPIO
 import requests
-
+from influxdb import InfluxDBClient
 import yaml
+import PID
+
 config = yaml.load(open("config.yaml"))
 
-from influxdb import InfluxDBClient
-client = InfluxDBClient(host=config['influx_host'],
-                        port=8086,
-                        database=config['influx_db'],
-                        timeout=10)
+client = InfluxDBClient(
+    host=config['influx_host'],
+    port=8086,
+    timeout=10,
+    database=config['influx_db'],
+    ssl=True,
+    verify_ssl=config['influx_host_verify_cert'],
+    username=config['influx_user'],
+    password=config['influx_password'],
+)
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
 
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
+
 
 def read_temp():
     lines = read_temp_raw()
@@ -30,31 +38,65 @@ def read_temp():
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
+        temp_string = lines[1][equals_pos + 2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT, initial=0)
 
-off_temp = config['set_temp'] + config['off_temp_offset']
-on_temp = config['set_temp'] + config['on_temp_offset']
+def pwm_heater_control_loop(state):
+    """PID controlled loop using PWM to control heat output"""
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(21, GPIO.OUT, initial=0)
+    GPIO.output(21, 0)
+    time.sleep(20)  # wait for other PID process to determine output
+    period = config['heatperiod']
+
+    while True:
+        if state['pid'] > 1:  # 100%
+            print("on")
+            GPIO.output(21, 1)
+            time.sleep(30)
+        elif state['pid'] <= 0:  # 0%
+            print("off")
+            GPIO.output(21, 0)
+            time.sleep(30)
+        else:
+            on_sec = state['pid'] * period
+            if on_sec >= period:
+                on_sec = period
+            if on_sec < 0:
+                on_sec = 0
+
+            print("on for {:3.2f}%% ({:3.2f} sec)".format(
+                on_sec / period * 100.0, on_sec))
+            GPIO.output(21, 1)
+            time.sleep(on_sec)
+
+            print("off for {:3.2f}%% ({:3.2f} sec)".format(
+                (period - on_sec) / period * 100.0, period - on_sec))
+            GPIO.output(21, 0)
+            time.sleep(period - on_sec)
 
 
-try:
+def pid_control(state):
+    pid = PID.PID(config['pid_P'], config['pid_I'], config['pid_D'])
+    pid.SetPoint = state['set_temp']
+    pid.setSampleTime(config['pid_sample_period'])
+    pid.setWindup(config['pid_windup'])
+
     while True:
         temp_c, temp_f = read_temp()
         print(temp_c, temp_f)
+        state['temp_f'] = temp_f
+        start = time.clock()
+        next = start + config['pid_sample_period']
 
-        # just a bit of leeway to avoid cycling too often.
-        if temp_f <= on_temp:
-            print("heat on")
-            heat = 1
-        elif temp_f >= off_temp:
-            print("heat off")
-            heat = 0
-        GPIO.output(21, heat)
+        pid.update(state['temp_f'])
+        print("pid.output={}".format(pid.output))
+        print("P,I,D={}".format(pid.pidterms()))
+        state['pid'] = pid.output
+
         json_body = []
         json_body.append({
             "measurement": "saltine.temp_f",
@@ -64,9 +106,43 @@ try:
                 "value": float(temp_f),
             }
         })
-        client.write_points(json_body)
-        requests.get(config['healthcheck_url'], timeout=10)
 
-        time.sleep(10)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+        try:
+            # sometimes network requests fail, but don't fail this loop
+            client.write_points(json_body)
+            requests.get(config['healthcheck_url'], timeout=10)
+        except Exception as e:
+            print(e)
+            pass
+
+        until_next_period = next - time.clock()
+        if until_next_period > 0:
+            time.sleep(until_next_period)
+
+
+if __name__ == '__main__':
+    from multiprocessing import Process, Manager
+    temp_c, temp_f = read_temp()
+
+    manager = Manager()
+    state = manager.dict()
+    state['on_temp'] = config['set_temp'] + config['on_temp_offset']
+    state['off_temp'] = config['set_temp'] + config['off_temp_offset']
+    state['temp_f'] = temp_f
+    state['set_temp'] = config['set_temp']
+    state['pid'] = 0
+
+    try:
+        h = Process(target=pwm_heater_control_loop, args=(state,))
+        h.daemon = True
+        h.start()
+
+        p = Process(target=pid_control, args=(state,))
+        p.daemon = True
+        p.start()
+
+        while h.is_alive() and p.is_alive():
+
+            time.sleep(5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
